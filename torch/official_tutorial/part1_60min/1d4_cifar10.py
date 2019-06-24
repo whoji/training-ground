16a6ddb947f971a7fd7a9503fc5b028486e58da1
whoji <minli1985@gmail.com>
2019-06-24 13:31:27 -0700

    [bug] fixed some typos. added dpu support

--- torch/official_tutorial/part1_60min/1d4_cifar10.py
+++ torch/official_tutorial/part1_60min/1d4_cifar10.py
@@ -6,11 +6,12 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 import torch.optim as opt
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device = "cpu"
 print("we are doing this on: "+str(device) + " !!!")
 
 transform = transforms.Compose(
@@ -30,21 +31,22 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-def not_runable():
-    def imshow(img):
-        img = img / 2 + 0.5 # unnormalize
-        np_img = img.numpy()
-        plt.imshow(np.transpose(np_img, (1,2,0)))
-        plt.show()
+# def not_runable():
+def imshow(img):
+    img = img / 2 + 0.5 # unnormalize
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1,2,0)))
+    plt.show()
 
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
 
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# show images
+print("NOW showing 1 sample")
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 class Net(nn.Module):
@@ -52,10 +54,10 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3,6,5)  # in 3 ; out 6 ; 5x5
         self.pool = nn.MaxPool2d(2,2)
-        self.Conv2d = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5* 5, 120)
-        self.fc1 = nn.Linear(120, 84)
-        self.fc1 = nn.Linear(84, 10)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -67,6 +69,7 @@ class Net(nn.Module):
         return x
 
 net = Net()
+net = net.to(device)
 LOSS = nn.CrossEntropyLoss()
 opt = opt.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
@@ -78,11 +81,14 @@ opt = opt.SGD(net.parameters(), lr=0.001, momentum=0.9)
 for epoch in range(2):
     running_loss =0.0
     for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        # inputs, labels = data.to(device)
+        #inputs, labels = data
+        #inputs = inputs.to(device)
+        #labels = labels.to(device)
+        inputs, labels = data[0].to(device), data[1].to(device)
         opt.zero_grad()
         outputs = net(inputs)
-        loss = LOSS(outputs, inputs)
+        #loss = LOSS(outputs, labels)
+        loss = LOSS(outputs, labels)
         loss.backward()
         opt.step()
 
@@ -95,11 +101,15 @@ print("GGWP")
 
 # test the test dataset
 
+print("NOW testing 1 sample")
 dataiter = iter(testloader)
-images, labels = dataiter.next()
+images_, labels = dataiter.next()
+images = images_.to(device)
+labels = labels.to(device)
+
 
 # print images
-imshow(torchvision.utils.make_grid(images))
+imshow(torchvision.utils.make_grid(images_))
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 outputs = net(images)
 _, predicted = torch.max(outputs, 1)
@@ -108,11 +118,13 @@ print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
                               for j in range(4)))
 
 # test on whole dataset
+print("NOW testing whole testing dataset")
 correct = 0
 total = 0
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
+        # images, labels = data
+        iamges, labels = data[0].to(device), data[1].to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
@@ -126,7 +138,8 @@ class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
+        # images, labels = data
+        iamges, labels = data[0].to(device), data[1].to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs, 1)
         c = (predicted == labels).squeeze()
@@ -142,5 +155,10 @@ for i in range(10):
 
 # question to clarify
 # 1. how to run these iterator and enumerator
+#   * done
 # 2. how to run on gpu
+#   * done
 # 3. torch.max dim
+# 4. loss(output, target) same as loss(target, output)
+#   * NO ! they are not the same
+#   * https://pytorch.org/docs/stable/nn.html
