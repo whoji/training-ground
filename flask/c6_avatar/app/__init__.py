ef4e72a81d88cbfb8da9368167747aa46383edb6
whoji <minli1985@gmail.com>
2019-09-17 12:02:26 -0700

    started c6

---
+++ flask/c6_avatar/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) # migration engine. wtf is this ?
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes
+from app import models # This module will define the structure of the database.
+'''
+The routes are the different URLs that the application implements.
+In Flask, handlers for the application routes are written as Python
+functions, called view functions. View functions are mapped to one
+ or more route URLs so that Flask knows what logic to execute when
+ a client requests a given URL.
+'''
+
