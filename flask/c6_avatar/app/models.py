6f031ad0416260b9f12381f16044debe7bf4abdf
whoji <minli1985@gmail.com>
2019-09-17 14:14:43 -0700

    finished chap6

--- flask/c6_avatar/app/models.py
+++ flask/c6_avatar/app/models.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 '''
 The model class created in the previous section defines the
@@ -16,6 +17,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
 
     def __repr__(self):
         return '<USER {}>'.format(self.username)
@@ -25,6 +29,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
