d485dbdd09b2b7f02d3835e881445b08efbf63d6
whoji <minli1985@gmail.com>
2019-10-28 14:25:17 -0700

    starting ch13

---
+++ flask/c13_babel/app/__init__.py
@@ -0,0 +1,70 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) # migration engine. wtf is this ?
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+mail = Mail(app)
+
+bootstrap = Bootstrap(app)
+
+moment = Moment(app)
+
+from app import routes
+from app import models # This module will define the structure of the database.
+'''
+The routes are the different URLs that the application implements.
+In Flask, handlers for the application routes are written as Python
+functions, called view functions. View functions are mapped to one
+ or more route URLs so that Flask knows what logic to execute when
+ a client requests a given URL.
+'''
+
+from app import errors
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
---
+++ flask/c13_babel/run.sh
@@ -0,0 +1,14 @@
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+
+# # to test the email notification
+# export FLASK_DEBUG=0
+# python -m smtpd -n -c DebuggingServer localhost:8025 &
+# export MAIL_SERVER=localhost
+# export MAIL_PORT=8025
+
+# export MAIL_SERVER=localhost
+# export MAIL_PORT=8025
+# python -m smtpd -n -c DebuggingServer localhost:8025
+
+flask run
