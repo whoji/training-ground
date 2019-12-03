c5b93a2e33bbf564bee1238581cf3ca173ce51a1
whoji <minli1985@gmail.com>
2019-12-03 15:55:44 -0800

    starting to work on 103

---
+++ shared_weight_nn/try_103.py
@@ -0,0 +1,101 @@
+# started 11.26.2019
+# ref 1 : https://github.com/pytorch/examples/blob/master/mnist/main.py
+# ref 2 : https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as opt
+import numpy as np
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+class NetShared(nn.Module):
+
+    def __init__(self):
+        super(NetShared, self).__init__()
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+class NetMain(nn.Module):
+
+    def __init__(self, shared_net):
+        super(NetMain, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.shared_net = shared_net
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        # x = self.fc1(x)
+        # x = F.relu(x)
+        # x = self.fc2(x)
+        # output = F.log_softmax(x, dim=1)
+        output = self.shared_net(x)
+        return output
+
+
+device = "cpu"
+
+N = 1000
+
+x = torch.randn(N, 1, 28, 28)
+y = torch.randn(N, 10)
+
+shared_model = NetShared().to(device)
+model_1 = NetMain(shared_model)
+model_2 = NetMain(shared_model)
+
+model_1 = Net()
+
+
+# Construct our loss function and an Optimizer. Training this strange model with
+# vanilla stochastic gradient descent is tough, so we use momentum
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model_1.parameters(), lr=1e-4, momentum=0.9)
+for t in range(500):
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model_1(x)
+
+    # Compute and print loss
+    loss = criterion(y_pred, y)
+    if t % 100 == 10:
+        print(t, loss.item())
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
