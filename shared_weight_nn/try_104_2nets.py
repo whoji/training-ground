b107cc47c4a04bb8868c410ab207bacab5a86e4c
whoji <minli1985@gmail.com>
2019-12-03 17:56:01 -0800

    now the true shared net is runnable...

--- shared_weight_nn/try_104_2nets.py
+++ shared_weight_nn/try_104_2nets.py
@@ -72,8 +72,12 @@ device = "cpu"
 
 N = 1000
 
-x = torch.randn(N, 1, 28, 28)
-y = torch.randint(0, 10, size=(N,))
+x_1 = torch.randn(N, 1, 28, 28)
+y_1 = torch.randint(0, 10, size=(N,))
+
+x_2 = torch.randn(N, 1, 28, 28)
+y_2 = torch.randint(0, 10, size=(N,))
+
 
 shared_model = NetShared().to(device)
 model_1 = NetMain(shared_model)
@@ -85,17 +89,23 @@ model_2 = NetMain(shared_model)
 # Construct our loss function and an Optimizer. Training this strange model with
 # vanilla stochastic gradient descent is tough, so we use momentum
 criterion = torch.nn.CrossEntropyLoss(reduction='mean')
-optimizer = torch.optim.SGD(model_1.parameters(), lr=0.01, momentum=0.9)
+optimizer_1 = torch.optim.SGD(model_1.parameters(), lr=0.01, momentum=0.9)
+optimizer_2 = torch.optim.SGD(model_2.parameters(), lr=0.01, momentum=0.9)
 for t in range(500):
     # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model_1(x)
+    y_pred_1 = model_1(x_1)
+    y_pred_2 = model_2(x_2)
 
     # Compute and print loss
-    loss = criterion(y_pred, y)
+    loss_1 = criterion(y_pred_1, y_1)
+    loss_2 = criterion(y_pred_2, y_2)
     if t % 10 == 0:
-        print(t, loss.item())
+        print(t, loss_1.item(), loss_2.item())
 
     # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    optimizer_1.zero_grad()
+    optimizer_2.zero_grad()
+    loss_1.backward()
+    loss_2.backward()
+    optimizer_1.step()
+    optimizer_2.step()
